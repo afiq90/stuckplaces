52949643370bb8c3d2b042d0b17812791816bd7d
Afiq <afiq@NSLs-MacBook-Air.local>
2015-07-10 14:54:32 +0800

    Added scores to review using raty

---
+++ app/models/place.rb
@@ -0,0 +1,14 @@
+class Place < ActiveRecord::Base
+	validates_presence_of :name
+	validates_presence_of :phone
+	validates_presence_of :website
+	validates_presence_of :user_id
+	validates_presence_of :address
+
+	belongs_to :user
+	geocoded_by :address
+	after_validation :geocode
+
+	has_many :reviews, dependent: :destroy
+
+end
